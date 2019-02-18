213ff316c3b53bda9e3967530ccb4e183dd18612
Jürgen Bayer <jb@juergen-bayer.net>
2019-02-18 19:30:35 +0000

    Updated the texts

--- src/ComponentWithPrompt.js
+++ src/ComponentWithPrompt.js
@@ -8,9 +8,9 @@ class ComponentWithPrompt extends Component {
       return (
          <div>
             <h1>Component with a Prompt</h1>
-            <div>If you came here via the Home component, try to use the browser's back button to go back to <NavLink to="/">Home</NavLink>.</div>
-            <div style={{marginTop: 10}}>The <NavLink to="/">Home</NavLink> link works, but only if the browser's back button was not clicked before. If the browser#s back button
-                 was clicked before, even the link does not work anymore.</div>
+            <div>If you came here in Safari on iOS via the Home component, try to use the browser's back button to go back to <NavLink to="/">Home</NavLink>.</div>
+            <div style={{marginTop: 10}}>The <NavLink to="/">Home</NavLink> link works, but only if the browser's back button was not clicked before. 
+                 If the browser's back button was clicked before, even the <NavLink to="/">Home</NavLink> link does not work anymore.</div>
             <Prompt
                when={true}
                message="Really navigate away?" />            
--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,10 @@ class Home extends Component {
       return (
          <div>
             <h1>Home</h1>
-            <div>When running the app, goto the <NavLink to="/pageWithPrompt">ComponentWithPromt</NavLink>, and then try to get back using the browser's back button first (and after this even over the Home link).</div>
-            <div style={{marginTop: 10}}>On iOS (12, and probably in Safari on MacOS) the Prompt component prevents the browser to go back and somehow after this even prevents the router link to work.</div>
+            <div>When running the app in Safari (iOS), goto the <NavLink to="/pageWithPrompt">ComponentWithPromt</NavLink>, and then 
+                 try to get back using the browser's back button first (and after this even by the Home link).</div>
+            <div style={{marginTop: 10}}>On iOS (12, and probably in Safari on MacOS) the Prompt component prevents the browser to go 
+                 back and somehow after this, even prevents the router link to work.</div>
          </div>
       );
    }
