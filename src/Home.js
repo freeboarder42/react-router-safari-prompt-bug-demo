4cd8489d805bd2d307b68d8fc1bb6f2ebeb08dc1
Jürgen Bayer <jb@juergen-bayer.net>
2019-02-19 10:06:07 +0000

    Minor text correction

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
             <h1>Home</h1>
             <div>When running the app in Safari (iOS), goto the <NavLink to="/pageWithHistoryBlock">component with history.block</NavLink>, and then 
                  try to get back using the browser's back button first (and after this even by the Home link).</div>
-            <div style={{marginTop: 10}}>On iOS (12, and probably in Safari on MacOS) the Prompt component does not display a prompt 
+            <div style={{marginTop: 10}}>On iOS (12, and probably also in Safari on MacOS) the Prompt component does not display a prompt 
                  and prevents the browser to go back. Additionally, after this, history.block even prevents the router link to work.</div>
          </div>
       );
